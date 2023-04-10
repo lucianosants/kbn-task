e24a8457ba0f8e002c7085ec96f205e24e0f09d4
lucianosants <lucianola18@gmail.com>
2023-04-10 15:56:24 -0300

    `feat` add nav component

---
+++ src/patterns/Nav/index.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+export default function Nav() {
+    return (
+        <header className=" bg-neutral-variant-700">
+            <div className="flex items-center justify-between p-6 mx-auto max-w-7xl">
+                <Link href="/">
+                    <h1 className="text-xl font-extrabold text-transparent hover:opacity-80 bg-gradient-to-r bg-clip-text from-primary-500 to-secondary-500 w-fit">
+                        KBN-TASK
+                    </h1>
+                </Link>
+
+                <div>
+                    <button
+                        type="button"
+                        className="px-3 py-2 font-semibold rounded-md hover:bg-primary-700 text-neutral-50 bg-primary-600"
+                        aria-label="Log in with your account"
+                    >
+                        Log in
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
