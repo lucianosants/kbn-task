6494c60a81d6b7b61d1960d3788032c84527789a
lucianosants <lucianola18@gmail.com>
2023-04-10 16:39:18 -0300

    `feat` moved all static data to local files

--- src/patterns/Nav/index.tsx
+++ src/patterns/Nav/index.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 
+import data from '@/_data/home/en.json';
+
 export default function Nav() {
     return (
         <header className=" bg-neutral-variant-700">
             <div className="flex items-center justify-between p-6 mx-auto max-w-7xl">
                 <Link href="/">
                     <h1 className="text-xl font-extrabold text-transparent hover:opacity-80 bg-gradient-to-r bg-clip-text from-primary-500 to-secondary-500 w-fit">
-                        KBN-TASK
+                        {data.header.brand}
                     </h1>
                 </Link>
 
@@ -16,7 +18,7 @@ export default function Nav() {
                         className="px-3 py-2 font-semibold rounded-md hover:bg-primary-700 text-neutral-50 bg-primary-600"
                         aria-label="Log in with your account"
                     >
-                        Log in
+                        {data.header.button_login}
                     </button>
                 </div>
             </div>
