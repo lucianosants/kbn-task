2570da5a508a23a272100f63e6720e7d6e25dda2
lucianosants <lucianola18@gmail.com>
2023-04-17 19:38:44 -0300

    `feat` add fetures for tasks as CRUD

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,36 +1,103 @@
-import { ReactNode, DragEvent } from 'react';
+import { ReactNode, DragEvent, useState, useEffect, ChangeEvent } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+
+const defaultStyles = 'p-3 rounded-xl cursor-pointer';
+
+const { content, status } = {
+    status: {
+        todo: {
+            container: `${defaultStyles} bg-danger-400/50 text-danger-100`,
+        },
+        doing: {
+            container: `${defaultStyles} bg-primary-300/50 text-primary-999 rounded-xl`,
+        },
+        done: {
+            container: `${defaultStyles} bg-secondary-600/50 text-secondary-50 rounded-xl`,
+        },
+    },
+    content: 'line-clamp-3',
+};
 
 interface TodoProps {
+    id: string;
     status: 'todo' | 'doing' | 'done';
     children: ReactNode;
-    onDragStart: (e: DragEvent) => void;
+    onDragStart: (event: DragEvent) => void;
+    editTask: ({ ...props }) => void;
+    onOutput: (event: string) => void;
+    readOnly: boolean;
 }
 
-export default function Todo(props: TodoProps) {
-    const defaultStyles = 'p-3 rounded-xl cursor-pointer';
+export default function Todo({ children, readOnly, ...props }: TodoProps) {
+    const [taskContent, setTaskContent] = useState(children);
+    const [isReadOnly, setIsReadOnly] = useState(readOnly);
 
-    const { content, status } = {
-        status: {
-            todo: {
-                container: `${defaultStyles} bg-danger-400/50 text-danger-100`,
-            },
-            doing: {
-                container: `${defaultStyles} bg-primary-300/50 text-primary-999 rounded-xl`,
-            },
-            done: {
-                container: `${defaultStyles} bg-secondary-600/50 text-secondary-50 rounded-xl`,
-            },
-        },
-        content: 'line-clamp-3',
+    const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const value = event.target.value;
+
+        setTaskContent(value);
+        props.onOutput(value);
     };
 
+    useEffect(() => {
+        setIsReadOnly(readOnly);
+    }, [readOnly]);
+
     return (
-        <article
-            className={status[props.status].container}
-            draggable
-            onDragStart={props.onDragStart}
-        >
-            <p className={content}>{props.children}</p>
-        </article>
+        <>
+            <Dialog.Root>
+                <Dialog.Trigger asChild>
+                    <article
+                        className={status[props.status].container}
+                        draggable
+                        onDragStart={props.onDragStart}
+                    >
+                        <p className={content}>{children}</p>
+                    </article>
+                </Dialog.Trigger>
+
+                <Dialog.Portal>
+                    <Dialog.Overlay className="fixed inset-0 w-screen h-full backdrop-blur-md bg-neutral-variant-700/40 " />
+
+                    <Dialog.Content className="fixed w-11/12 max-w-2xl p-6 -translate-x-1/2 -translate-y-1/2 border border-neutral-900 sm:p-10 top-80 left-1/2 bg-neutral-variant-600/80 backdrop-blur-xl rounded-xl">
+                        <textarea
+                            className="w-full p-3 resize-none h-72 sm:h-48 text-primary-50 bg-neutral-variant-800/60 backdrop-blur-2xl rounded-xl"
+                            readOnly={isReadOnly}
+                            value={taskContent as string}
+                            onChange={handleChange}
+                        />
+
+                        <div className="flex justify-end gap-3 mt-6">
+                            <Dialog.Close
+                                className="p-2 border rounded-xl hover:bg-neutral-800 text-primary-500 border-primary-500"
+                                aria-label="Close form"
+                            >
+                                <span>Close</span>
+                            </Dialog.Close>
+
+                            <button
+                                className={`p-3 ${
+                                    isReadOnly
+                                        ? 'bg-primary-600'
+                                        : 'bg-secondary-600 text-neutral-variant-900'
+                                }  bg-primary-600 rounded-xl text-neutral-variant-50 hover:bg-primary-700"
+                                aria-label="Close form`}
+                                onClick={props.editTask}
+                            >
+                                <span>{isReadOnly ? 'Edit' : 'Save'}</span>
+                            </button>
+
+                            <button
+                                className="p-2 bg-danger-600 rounded-xl text-neutral-variant-50 hover:bg-danger-700 disabled:opacity-50"
+                                aria-label="Close form"
+                                disabled={!isReadOnly}
+                            >
+                                <span>Delete</span>
+                            </button>
+                        </div>
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+        </>
     );
 }
